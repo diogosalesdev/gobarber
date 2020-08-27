ac0485bcb9df2a2b6d5463888f96d24c658dd84c
Diogo Sales <diogosalesdev@gmail.com>
2020-08-27 11:06:31 -0300

    Record creation functionality created

--- backend/src/routes/appointments.routes.ts
+++ backend/src/routes/appointments.routes.ts
@@ -6,7 +6,6 @@ import AppointmentsRepository from '../repositories/AppointmentsRepository';
 import CreateAppointmentService from '../services/CreateAppointmentService';
 
 const appointmentsRouter = Router();
-const appointmentsRepository =  new AppointmentsRepository();
 
 appointmentsRouter.get('/', async (request, response) => {
   const appointmentsRepository = getCustomRepository(AppointmentsRepository);
@@ -18,13 +17,13 @@ appointmentsRouter.get('/', async (request, response) => {
 
 appointmentsRouter.post('/', async (request, response) => {
   try {
-    const { provider, date } = request.body;
+    const { provider_id, date } = request.body;
 
     const parsedDate = parseISO(date);
 
     const createAppointment = new CreateAppointmentService();
 
-    const appointment = await createAppointment.execute({date: parsedDate, provider})
+    const appointment = await createAppointment.execute({date: parsedDate, provider_id})
 
     return response.json(appointment);
   } catch (err) {
