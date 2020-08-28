7e3cb9cde71f4d086a423fcb8ad61660465ab4b5
Diogo Sales <diogosalesdev@gmail.com>
2020-08-28 15:35:42 -0300

    Error class created

--- backend/src/services/CreateAppointmentService.ts
+++ backend/src/services/CreateAppointmentService.ts
@@ -3,6 +3,7 @@ import AppointmentsRepository from '../repositories/AppointmentsRepository';
 import { getCustomRepository } from 'typeorm';
 
 import Appointment from '../models/Appointment';
+import AppError from '../errors/AppError';
 
 interface Request {
   provider_id: string;
@@ -18,7 +19,7 @@ class CreateAppointmentService {
     const findAppointmentInSameDate = await appointmentsRepository.findByDate(appointmentDate);
 
     if (findAppointmentInSameDate) {
-      throw Error('This appointment is already booked!');
+      throw new AppError('This appointment is already booked!');
     }
 
     const appointment = appointmentsRepository.create({
--- backend/src/services/UpdateUserAvatarService.ts
+++ backend/src/services/UpdateUserAvatarService.ts
@@ -4,6 +4,7 @@ import fs from 'fs';
 
 import uploadConfig from '../config/upload';
 import User from "../models/User";
+import AppError from "../errors/AppError";
 
 interface Request {
   user_id: string;
@@ -15,7 +16,7 @@ class UpadateUserAvatarService {
     const usersRepository = getRepository(User);
     const user = await usersRepository.findOne(user_id);
     if (!user) {
-      throw new Error('Only authenticated users can change avatar!');
+      throw new AppError('Only authenticated users can change avatar!', 401);
     }
     if (user.avatar) {
       const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
