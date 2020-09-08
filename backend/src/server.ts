fe81461a1f9502219206630deeb11e026a2d37c1
Diogo Sales <diogosalesdev@gmail.com>
2020-09-08 15:12:47 -0300

    Cors habilited in the API

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 
 import express, { Response, Request, NextFunction } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 import routes from './routes';
 import uploadConfig from './config/upload';
@@ -10,10 +11,9 @@ import AppError from './errors/AppError';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-
 app.use('/files', express.static(uploadConfig.directory));
-
 app.use(routes);
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
