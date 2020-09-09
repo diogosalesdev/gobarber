6f6acdb8f59beeabc82047b44372613742798562
Diogo Sales <diogosalesdev@gmail.com>
2020-09-09 15:27:42 -0300

    App logout created

--- frontend/src/pages/SignIn/index.tsx
+++ frontend/src/pages/SignIn/index.tsx
@@ -4,7 +4,7 @@ import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
 
-import { useAuth } from '../../context/AuthContext';
+import { useAuth } from '../../hooks/AuthContext';
 import getValidationErrors from '../../utils/getValidationErrors';
 
 import logoImg from '../../assets/logo.svg';
@@ -23,8 +23,6 @@ const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
   const { signIn } = useAuth();
-
-  console.log(user);
 
   const handleSubmit = useCallback(async (data: SignInFormData) => {
     try {
