1c0f19c700fc34515450bb0d05f3185de22a8ff1
Diogo Sales <diogosalesdev@gmail.com>
2020-10-04 18:04:34 -0300

    Amazon S3 upload files created

--- backend/src/modules/users/infra/http/routes/users.routes.ts
+++ backend/src/modules/users/infra/http/routes/users.routes.ts
@@ -9,7 +9,7 @@ import UsersAvatarController from '../controllers/UsersAvatarContrroller';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
-const upload = multer(uploadConfig);
+const upload = multer(uploadConfig.multer);
 
 const usersController = new UsersController();
 const usersAvatarController = new UsersAvatarController();
